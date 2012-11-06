a151af0c019d58007c9ad736b3492ed9f5208c7f
Xuerian <sky.shell@gmail.com>
2012-11-06 07:28:23 -0500

    Correctly target core localization at root namespace only

--- localization.lua
+++ localization.lua
@@ -38,15 +38,15 @@ local locales = {
 
 -- Automatically inserted translations
 -- See: http://wow.curseforge.com/addons/xloot-1-0/localization/ to create or fix translations
---@localization(locale="ptBR", format="lua_additive_table", table-name="locales.ptBR", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="frFR", format="lua_additive_table", table-name="locales.frFR", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="deDE", format="lua_additive_table", table-name="locales.deDE", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="koKR", format="lua_additive_table", table-name="locales.koKR", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="esMX", format="lua_additive_table", table-name="locales.esMX", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="ruRU", format="lua_additive_table", table-name="locales.ruRU", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="zhCN", format="lua_additive_table", table-name="locales.zhCN", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="esES", format="lua_additive_table", table-name="locales.esES", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
---@localization(locale="zhTW", format="lua_additive_table", table-name="locales.zhTW", handle-subnamespaces="subtable", handle-unlocalized="ignore")@
+--@localization(locale="ptBR", format="lua_additive_table", table-name="locales.ptBR", handle-unlocalized="ignore")@
+--@localization(locale="frFR", format="lua_additive_table", table-name="locales.frFR", handle-unlocalized="ignore")@
+--@localization(locale="deDE", format="lua_additive_table", table-name="locales.deDE", handle-unlocalized="ignore")@
+--@localization(locale="koKR", format="lua_additive_table", table-name="locales.koKR", handle-unlocalized="ignore")@
+--@localization(locale="esMX", format="lua_additive_table", table-name="locales.esMX", handle-unlocalized="ignore")@
+--@localization(locale="ruRU", format="lua_additive_table", table-name="locales.ruRU", handle-unlocalized="ignore")@
+--@localization(locale="zhCN", format="lua_additive_table", table-name="locales.zhCN", handle-unlocalized="ignore")@
+--@localization(locale="esES", format="lua_additive_table", table-name="locales.esES", handle-unlocalized="ignore")@
+--@localization(locale="zhTW", format="lua_additive_table", table-name="locales.zhTW", handle-unlocalized="ignore")@
 
 
 XLoot.L = CompileLocales(locales)
