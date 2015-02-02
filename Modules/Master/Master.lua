c023eb23d0521b84e8bddc8c4d009429890909a2
Xuerian <sky.shell@gmail.com>
2015-02-02 07:20:18 -0500

    Master: Use XLoot.Deformat instead of a simple gsub set to match RANDOM_ROLL_RESULT

--- Modules/Master/Master.lua
+++ Modules/Master/Master.lua
@@ -169,9 +169,9 @@ function addon.RaidRoll(frame, players)
  		RandomRoll(1,to)
  	end
 end
-local rollCapture = gsub(gsub(gsub(RANDOM_ROLL_RESULT, "[()]", "."), "%%s", "([^ ]+)") , "%%d", "(%%d+)")
+
 function addon:CHAT_MSG_SYSTEM(...)
-	local who, roll, from, to = match(..., rollCapture)
+	local who, roll, from, to = XLoot.Deformat(..., RANDOM_ROLL_RESULT)
 	if who == me then
 		eframe.value = tonumber(roll)
 		eframe:UnregisterEvent("CHAT_MSG_SYSTEM")
