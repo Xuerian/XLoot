bcb9d18920d10131d56cf95ba925ecd667eff4bc
Xuerian <sky.shell@gmail.com>
2016-02-08 18:59:33 -0500

    group: Disabled hook_alert/hook_bonus options due to possibly breaking bonus rolls. If you want these options back and are willing to accept the risk, you can enable unsafe_hook_alert and unsafe_hook_bonus in your saved variables.

--- Modules/Options/Options.lua
+++ Modules/Options/Options.lua
@@ -554,12 +554,12 @@ function addon:OnEnable() -- Construct addon option tables here
 				{ "roll_anchor_visible", "toggle", "roll_anchor", "visible", set = set_anchor },
 				{ "alert_anchor_visible", "toggle", "alert_anchor", "visible", set = set_anchor, width = "double" },
 			}},
-			{ "other_frames", "group", {
-				{ "hook_bonus" },
-				{ "bonus_skin", requires = "hook_bonus", width = "double" },
-				{ "hook_alert" },
-				{ "alert_skin", requires = "hook_alert", width = "double" },
-			}},
+			-- { "other_frames", "group", {
+			-- 	{ "hook_bonus" },
+			-- 	{ "bonus_skin", requires = "hook_bonus", width = "double" },
+			-- 	{ "hook_alert" },
+			-- 	{ "alert_skin", requires = "hook_alert", width = "double" },
+			-- }},
 			{ "rolls", "group", {
 				{ "roll_direction", "select", directions, "roll_anchor", "direction" , name = L.growth_direction },
 				{ "text_outline", "toggle" },
