b8f1eab0004500a3605fbe257f37c22e92694065
wheelbarrel00 <103789673+wheelbarrel00@users.noreply.github.com>
2026-07-11 20:25:19 -0700

    Fix 12.12.0 packaging: drop Toast CurseForge localization export (Toast namespace not registered on the project)

--- Modules/Toast/localization.lua
+++ Modules/Toast/localization.lua
@@ -35,15 +35,4 @@ local locales = {
 	},
 }
 
--- Automatically inserted translations
---@localization(locale="ptBR", format="lua_additive_table", table-name="locales.ptBR", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="frFR", format="lua_additive_table", table-name="locales.frFR", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="deDE", format="lua_additive_table", table-name="locales.deDE", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="koKR", format="lua_additive_table", table-name="locales.koKR", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="esMX", format="lua_additive_table", table-name="locales.esMX", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="ruRU", format="lua_additive_table", table-name="locales.ruRU", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="zhCN", format="lua_additive_table", table-name="locales.zhCN", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="esES", format="lua_additive_table", table-name="locales.esES", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
---@localization(locale="zhTW", format="lua_additive_table", table-name="locales.zhTW", handle-subnamespaces="subtable", handle-unlocalized="ignore", namespace="Toast")@
-
 XLoot:Localize("Toast", locales)
